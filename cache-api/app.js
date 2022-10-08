c18e465a365a51f6f17ea348ce3f92f3c1aefc92
Rafael Vilas Boas <rafaelpiresvb@gmail.com>
2022-10-08 12:06:47 -0300

    feat: cache image data

--- cache-api/app.js
+++ cache-api/app.js
@@ -8,29 +8,37 @@ async function addToCache(requestUrl, response) {
 async function fetchFromNetwork(requestUrl) {
     const response = await fetch(requestUrl);
     addToCache(requestUrl, response.clone());
-    const json = await response.json();
-    return json;
+    return response;
 }
 
 async function fetchFromCache(requestUrl) {
     const cache = await caches.open(CACHE_KEY);
     const cachedResponse = await cache.match(requestUrl);
-    const pokeData = cachedResponse?.json();
-    return pokeData;
+    return cachedResponse || null;
 }
 
 async function showPokemonData() {
     const img = document.querySelector('img');
     const details = document.querySelector('details');
     const pre = document.querySelector('pre');
-    const requestUrl = `https://pokeapi.co/api/v2/pokemon/791`;
 
-    const pokeData = (await fetchFromCache(requestUrl)) || (await fetchFromNetwork(requestUrl));
+    const pokeNumber = Math.floor(Math.random() * 10) + 1;
+    const requestUrl = `https://pokeapi.co/api/v2/pokemon/${pokeNumber}`;
+
+    const pokeDataResponse = (await fetchFromCache(requestUrl)) || (await fetchFromNetwork(requestUrl));
+
+    const pokeData = await pokeDataResponse.json();
 
     pre.textContent = JSON.stringify(pokeData, null, 2);
-    img.src = pokeData.sprites.other['official-artwork'].front_default;
+
     img.alt = pokeData.name;
     img.title = pokeData.name;
+
+    const pokeImageUrl = pokeData.sprites.other['official-artwork'].front_default;
+    const pokeImageRequest = (await fetchFromCache(pokeImageUrl)) || (await fetchFromNetwork(pokeImageUrl));
+
+    img.src = URL.createObjectURL(await pokeImageRequest.blob());
+    // img.src = pokeData.sprites.other['official-artwork'].front_default;
 
     details.hidden = false;
 }
